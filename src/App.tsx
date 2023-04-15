a0656badfa39a2f757d21b9ddfad3c7044a6ef2e
YU01BLC <bgc00magenta@gmail.com>
2023-04-15 11:47:06 +0900

    update: 不要な箇所を削除し、必要なコンポーネントをインポート

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@chakra-ui/react';
+import ProteinCount from './component/proteinCount';
+import ProteinMeter from './component/protainMeter';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <ProteinCount />
+      <ProteinMeter />
+    </Box>
   );
 }
-
-export default App;
